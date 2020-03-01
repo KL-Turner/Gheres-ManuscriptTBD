5f4dc924fc5b45937bcc912abab2e713694368f8
KL-Turner <klt8@psu.edu>
2020-03-01 13:26:27 -0500

    .

--- New Model Code/CreateTrainingData_GT.m
+++ New Model Code/CreateTrainingData_GT.m
@@ -1,30 +1,31 @@
 % pull list of raw data files from current directory
-rawDataFileStruct = dir('*_RawData.mat');
-rawDataFiles = {rawDataFileStruct.name}';
-rawDataFileIDs = char(rawDataFiles);
+scoringDataFileStruct = dir('*_SleepScoringData.mat');
+scoringDataFiles = {scoringDataFileStruct.name}';
+scoringDataFileIDs = char(scoringDataFiles);
 % load each file and create a histogram of the EMG values
-% emgHold = [];
-% for a = 1:size(rawDataFileIDs,1)
-%     disp(['Gathering EMG data for probability distribution: (' num2str(a) '/' num2str(size(rawDataFileIDs,1)) ')']); disp(' ')
-%     rawDataFileID = rawDataFileIDs(a,:);
-%     load(rawDataFileID)
-%     emgHold = cat(2,emgHold,RawData.MUA);
-% end
-% % plot histogram of EMG data to assist in identification of bimodal distribution values
-% figure
-% histogram(emgHold,200,'Normalization','probability')
-% xlabel('EMG Power')
-% ylabel('Probability')
+emgHold = [];
+for a = 1:size(scoringDataFileIDs,1)
+    disp(['Gathering EMG data for probability distribution: (' num2str(a) '/' num2str(size(scoringDataFileIDs,1)) ')']); disp(' ')
+    scoringDataFileID = scoringDataFileIDs(a,:);
+    load(scoringDataFileID,'Ephys')
+    emgHold = cat(2,emgHold,Ephys.downSampleEMG);
+end
+% plot histogram of EMG data to assist in identification of bimodal distribution values
+figure
+histogram(emgHold,100,'Normalization','probability')
+xlabel('EMG Power')
+ylabel('Probability')
 % go through each file and create a list of manual scores
-for a = 1:size(rawDataFileIDs,1)
-    rawDataFileID = rawDataFileIDs(a,:);
-    disp(['Loading ' rawDataFileID ' for manual sleep scoring: (' num2str(a) '/' num2str(size(rawDataFileIDs,1)) ')' ]); disp(' ')
-    trainingDataFileID = [rawDataFileID(1:end-11) 'TrainingData.mat'];
+for a = 1:size(scoringDataFileIDs,1)
+    scoringDataFileID = scoringDataFileIDs(a,:);
+    disp(['Loading ' scoringDataFileID ' for manual sleep scoring: (' num2str(a) '/' num2str(size(scoringDataFileIDs,1)) ')' ]); disp(' ')
+    strBreaks = strfind(scoringDataFileID,'_');
+    trainingDataFileID = [scoringDataFileID(1:strBreaks(end)) 'TrainingData.mat'];
     if ~exist(trainingDataFileID,'file')
-        load(rawDataFileID)
+        load(scoringDataFileID)
         % need to add baseline structure or equivalent to figure function
-        [figHandle] = GenerateSingleFigures_GT(RawData);
-        trialDuration = round(length(RawData.vBall)/RawData.an_fs);
+        [figHandle] = GenerateSingleFigures_GT(AcquisitionParams.downSampled_Fs,Behavior.ballVelocity,Ephys.downSampleEMG,IOS.barrels.dHbT,Spectrograms.FiveSec);
+        trialDuration = round(length(Behavior.ballVelocity)/AcquisitionParams.downSampled_Fs);
         % determine number of 5 seconds bins
         numBins = trialDuration/5;
         behavioralState = cell(numBins,1);
@@ -35,14 +36,18 @@ for a = 1:size(rawDataFileIDs,1)
             xEndVal = b*5;
             xInds = xStartVal:1:xEndVal;
             figHandle = gcf;
-            subplot(2,1,1)
+            subplot(3,1,1)
             leftEdge1 = xline(xInds(1),'color',colors_GT('electric purple'),'LineWidth',2);
             hold on
             rightEdge1 = xline(xInds(5),'color',colors_GT('electric purple'),'LineWidth',2);
-            subplot(2,1,2)
+            subplot(3,1,2)
             leftEdge2 = xline(xInds(1),'color',colors_GT('electric purple'),'LineWidth',2);
             hold on
-            rightEdge2 = xline(xInds(5),'color',colors_GT('electric purple'),'LineWidth',2);            
+            rightEdge2 = xline(xInds(5),'color',colors_GT('electric purple'),'LineWidth',2);
+            subplot(3,1,3)
+            leftEdge3 = xline(xInds(1),'color',colors_GT('electric purple'),'LineWidth',2);
+            hold on
+            rightEdge3 = xline(xInds(5),'color',colors_GT('electric purple'),'LineWidth',2);
             % make decision
             [updatedGUI] = SelectBehavioralStateGUI_GT;
             while buttonState == 0
@@ -66,8 +71,10 @@ for a = 1:size(rawDataFileIDs,1)
             end
         delete(leftEdge1)
         delete(leftEdge2)
+        delete(leftEdge3)
         delete(rightEdge1)
         delete(rightEdge2)
+        delete(rightEdge3)
         end
         close(figHandle)
         TrainingTable.behavState = behavioralState;
--- New Model Code/GenerateSingleFigures_GT.m
+++ New Model Code/GenerateSingleFigures_GT.m
@@ -1,4 +1,4 @@
-function [figHandle] = GenerateSingleFigures_GT(RawData)
+function [figHandle] = GenerateSingleFigures_GT(samplingRate,ballVelocity,EMG,dHbT,SpecData)
 %________________________________________________________________________________________________________________________
 % Written by Kevin L. Turner
 % The Pennsylvania State University, Dept. of Biomedical Engineering
@@ -7,26 +7,54 @@ function [figHandle] = GenerateSingleFigures_GT(RawData)
 %   Purpose: Generate figure for visualized sleep scoring
 %________________________________________________________________________________________________________________________
 
-%% load/process data
-% ball velocity (raw)
-trialDuration = round(length(RawData.vBall)/RawData.an_fs);
-ballVelocity = RawData.vBall(1:trialDuration*RawData.an_fs);
-anTimeVec = (1:length(ballVelocity))/RawData.an_fs;
-
-% hemodynamics
-[B,A] = butter(3,1/(RawData.dal_fr/2),'low');
-barrelsRefl = filtfilt(B,A,RawData.IOS.barrels.CBVrefl(1:trialDuration*RawData.dal_fr));
-dsTimeVec = (1:length(barrelsRefl))/RawData.dal_fr;
-% Hippocampal LFP
+%% prepare data
+trialDuration = round(length(ballVelocity)/samplingRate);
+timeVec = (1:length(ballVelocity))/samplingRate;
+[B,A] = butter(3,1/(samplingRate/2),'low');
+% ball velocity - apply any extra processing or filters
+ballVelocity = ballVelocity(1:trialDuration*samplingRate); 
+% EMG - apply any extra processing or filters
+EMG = EMG(1:trialDuration*samplingRate); 
+% change in total hemoglobin
+procHbT = filtfilt(B,A,dHbT(1:trialDuration*samplingRate));
+% hippocampal LFP
+S = SpecData.S5_Norm*100;
+T = SpecData.T5;
+F = SpecData.F5;
 
 %% Figure
 figHandle = figure;
-ax1 = subplot(2,1,1);
-plot(anTimeVec,ballVelocity,'LineWidth',1,'color','k');
-
-ax2 = subplot(2,1,2);
-plot(dsTimeVec,barrelsRefl,'LineWidth',1,'color','r');
-
+% ball velocity and EMG
+ax1 = subplot(3,1,1);
+yyaxis left
+plot(timeVec,ballVelocity,'LineWidth',1,'color',colors_GT('sapphire'));
+ylabel('Ball velocity')
+yyaxis right
+plot(timeVec,EMG,'LineWidth',1,'color',colors_GT('deep carrot orange'));
+ylabel('EMG')
+% dHbT
+ax2 = subplot(3,1,2);
+plot(timeVec,procHbT,'LineWidth',1,'color',colors_GT('dark candy apple red'));
+ylabel('\DeltaHbT (\muM)')
+% hippocampal spec
+ax3 = subplot(3,1,3);
+SemiLogImageSC_GT(T,F,S,'y')
+c8 = colorbar;
+ylabel(c8,'\DeltaP/P (%)')
+caxis([-100,200])
+xlabel('Time (s)')
+ylabel({'Hippocampal LFP';'Frequency (Hz)'})
+xlim([0,trialDuration])
+set(gca,'TickLength',[0,0])
+set(gca,'box','off')
+% align axes in position after colorbar
+ax1Pos = get(ax1,'position');
+ax2Pos = get(ax2,'position');
+ax3Pos = get(ax3,'position');
+ax2Pos(3:4) = ax1Pos(3:4);
+ax3Pos(3:4) = ax1Pos(3:4);
+set(ax2,'position',ax2Pos);
+set(ax3,'position',ax3Pos);
 linkaxes([ax1,ax2],'x')
 
 end
