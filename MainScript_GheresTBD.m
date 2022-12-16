f6bc0042506c7a4375ccaf9c3d753ce88e286eff
Turner Jr <klt8@psu.edu>
2022-12-16 16:14:01 -0500

    .

--- MainScript_GheresTBD.m
+++ MainScript_GheresTBD.m
@@ -25,12 +25,12 @@ addpath(genpath(rootFolder))
 %% run the data analysis. The progress bars will show the analysis progress.
 rerunAnalysis = 'y';
 if exist('AnalysisResults.mat') ~= 2 || strcmp(rerunAnalysis,'y') == true %#ok<EXIST>
-    multiWaitbar_GheresTBD('Analyzing evoked responses',0,'Color','G'); pause(0.25);
-    multiWaitbar_GheresTBD('Analyzing behavioral transitions',0,'Color','G'); pause(0.25);
-    multiWaitbar_GheresTBD('Analyzing peri-stimulus arousal probability',0,'Color','G'); pause(0.25);
+    multiWaitbar('Analyzing evoked responses',0,'Color','G'); pause(0.25);
+    multiWaitbar('Analyzing behavioral transitions',0,'Color','G'); pause(0.25);
+    multiWaitbar('Analyzing peri-stimulus arousal probability',0,'Color','G'); pause(0.25);
     % run analysis and output a structure containing all the analyzed data.
-    [AnalysisResults] = AnalyzeData_Manuscript2020(rootFolder);
-    multiWaitbar_GheresTBD('CloseAll');
+    [AnalysisResults] = AnalyzeData_GheresTBD(rootFolder);
+    multiWaitbar('CloseAll');
 else
     disp('Loading analysis results and generating figures...'); disp(' ')
     load('AnalysisResults.mat')
@@ -39,7 +39,7 @@ end
 Turner_AdultStimFigure(rootFolder,'y',AnalysisResults)
 end
 
-function [AnalysisResults] = AnalyzeData_Manuscript2020(rootFolder)
+function [AnalysisResults] = AnalyzeData_GheresTBD(rootFolder)
 animalIDs = {'T99','T101','T102','T103','T105','T108','T109','T110','T111','T119','T120','T121','T122','T123'};
 if exist('AnalysisResults.mat') == 2 %#ok<EXIST>
     load('AnalysisResults.mat')
@@ -50,9 +50,9 @@ end
 runFromStart = 'n';
 for pp = 1:length(animalIDs)
     if isfield(AnalysisResults,(animalIDs{1,pp})) == false || isfield(AnalysisResults.(animalIDs{1,pp}),'EvokedAvgs') == false || strcmp(runFromStart,'y') == true
-        [AnalysisResults] = AnalyzeEvokedResponses2_GheresTBD(animalIDs{1,pp},rootFolder,AnalysisResults);
+        [AnalysisResults] = AnalyzeEvokedResponses_GheresTBD(animalIDs{1,pp},rootFolder,AnalysisResults);
     end
-    multiWaitbar_GheresTBD('Analyzing evoked responses','Value',pp/length(animalIDs));
+    multiWaitbar('Analyzing evoked responses','Value',pp/length(animalIDs));
 end
 %% Block [2] Analyze the transitions between different arousal-states (IOS)
 runFromStart = 'n';
@@ -60,7 +60,7 @@ for dd = 1:length(animalIDs)
     if isfield(AnalysisResults,(animalIDs{1,dd})) == false || isfield(AnalysisResults.(animalIDs{1,dd}),'Transitions') == false || strcmp(runFromStart,'y') == true
         [AnalysisResults] = AnalyzeTransitionalAverages_GheresTBD(animalIDs{1,dd},rootFolder,AnalysisResults);
     end
-    multiWaitbar_GheresTBD('Analyzing behavioral transitions','Value',dd/length(animalIDs));
+    multiWaitbar('Analyzing behavioral transitions','Value',dd/length(animalIDs));
 end
 %% Block [3] Analyze the peri-stimulus arousal state (IOS)
 runFromStart = 'n';
@@ -68,6 +68,6 @@ for dd = 1:length(animalIDs)
     if isfield(AnalysisResults,(animalIDs{1,dd})) == false || isfield(AnalysisResults.(animalIDs{1,dd}),'Probability') == false || strcmp(runFromStart,'y') == true
         [AnalysisResults] = AnalyzeStimulusArousalProbability_GheresTBD(animalIDs{1,dd},rootFolder,AnalysisResults);
     end
-    multiWaitbar_GheresTBD('Analyzing peri-stimulus arousal probability','Value',dd/length(animalIDs));
+    multiWaitbar('Analyzing peri-stimulus arousal probability','Value',dd/length(animalIDs));
 end
 end
